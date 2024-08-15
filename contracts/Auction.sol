70ff4e76a2249ac0d00bf91ef425cd9101a0ec8c
Lécio Oliveira Gonçalves de Assis <lecio.assis@unifesp.br>
2024-08-14 21:36:05 -0300

    feat(Winner Claim): add Winner Claim function

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -1,26 +1,36 @@
-contract Auction {
-    // static
-    address public owner;
-    uint public startBlock;
-    uint public endBlock;
-    string public ipfsHash;
-    uint public bidIncrement;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.26;
 
-    // state
+contract Auction {
+    // Static state variables
+    address public immutable owner;
+    uint public immutable startBlock;
+    uint public immutable endBlock;
+    string public ipfsHash;
+    uint public immutable bidIncrement;
+
+    // Dynamic state variables
     bool public canceled;
     address public highestBidder;
     mapping(address => uint256) public fundsByBidder;
     uint public highestBindingBid;
-    bool ownerHasWithdrawn;
+    bool public ownerHasWithdrawn;
+    bool public winnerHasClaimed;
 
-    event LogBid(address bidder, uint bid, address highestBidder, uint highestBid, uint highestBindingBid);
-    event LogWithdrawal(address withdrawer, address withdrawalAccount, uint amount);
+    event LogBid(address indexed bidder, uint bid, address indexed highestBidder, uint highestBid, uint highestBindingBid);
+    event LogWithdrawal(address indexed withdrawer, address indexed withdrawalAccount, uint amount);
     event LogCanceled();
 
-    constructor(address _owner, uint _bidIncrement, uint _startBlock, uint _endBlock, string memory _ipfsHash) {
+    constructor(
+        address _owner,
+        uint _bidIncrement,
+        uint _startBlock,
+        uint _endBlock,
+        string memory _ipfsHash
+    ) {
         require(_startBlock < _endBlock, "Ending Block must be greater than Starting Block.");
-        require(_startBlock >= block.number, "Starting Block must be greater or equal than the Current Block number.");
-        require(owner != address(0), "Owner address cannot be zero address.");
+        require(_startBlock >= block.number, string(abi.encodePacked("Starting Block must be greater or equal to the Current Block number.")));
+        require(_owner != address(0), "Owner address cannot be zero address.");
 
         owner = _owner;
         bidIncrement = _bidIncrement;
@@ -29,70 +39,110 @@ contract Auction {
         ipfsHash = _ipfsHash;
     }
 
-    function getHighestBid() 
-        internal 
-        returns (uint) 
-    {
+    function getHighestBid() public view returns (uint) {
         return fundsByBidder[highestBidder];
     }
 
-    function placeBid()
-        public
-        payable
-        onlyAfterStart
-        onlyBeforeEnd
-        onlyNotCanceled
-        onlyNotOwner
-        returns (bool success)
-    {
-       
+    function placeBid() external payable onlyAfterStart onlyBeforeEnd onlyNotCanceled onlyNotOwner returns (bool success) {
+        require(msg.value > 0, "Bid amount must be greater than zero.");
+
+        uint newBid = fundsByBidder[msg.sender] + msg.value;
+        require(newBid > highestBindingBid, "Bid must be greater than the highest binding bid.");
+
+        uint highestBid = fundsByBidder[highestBidder];
+        fundsByBidder[msg.sender] = newBid;
+
+        if (newBid <= highestBid) {
+            highestBindingBid = min(newBid + bidIncrement, highestBid);
+        } else {
+            if (msg.sender != highestBidder) {
+                highestBidder = msg.sender;
+                highestBindingBid = min(newBid, highestBid + bidIncrement);
+            }
+        }
+
+        emit LogBid(msg.sender, newBid, highestBidder, highestBid, highestBindingBid);
+        return true;
     }
 
-    function withdraw() 
-        internal
-        onlyEndedOrCanceled
-        returns (bool success) 
-    {
+    function withdraw() external onlyEndedOrCanceled returns (bool) {
+        uint withdrawalAmount;
+        address withdrawalAccount = msg.sender;
+
+        if (canceled) {
+            withdrawalAmount = fundsByBidder[withdrawalAccount];
+        } else if (msg.sender == owner) {
+            withdrawalAccount = highestBidder;
+            withdrawalAmount = highestBindingBid;
+            ownerHasWithdrawn = true;
+        } else if (msg.sender == highestBidder) {
+            withdrawalAmount = fundsByBidder[highestBidder] - (ownerHasWithdrawn ? highestBindingBid : 0);
+        } else {
+            withdrawalAmount = fundsByBidder[withdrawalAccount];
+        }
+
+        require(withdrawalAmount > 0, "Withdrawal amount must be greater than zero.");
+        fundsByBidder[withdrawalAccount] -= withdrawalAmount;
         
+        (bool transferSuccess, ) = payable(withdrawalAccount).call{value: withdrawalAmount}("");
+        require(transferSuccess, "Transfer failed.");
+
+        emit LogWithdrawal(msg.sender, withdrawalAccount, withdrawalAmount);
+        return true;
     }
 
-    function cancelAuction() 
-        internal
-        onlyOwner
-        onlyBeforeEnd
-        onlyNotCanceled
-        returns (bool success) 
-    {
-        
+    function claimItem () external view onlyWinner returns (bool) {
+        winnerHasClaimed == true;
+        return true;
     }
 
-    modifier onlyOwner {
-        require(msg.sender == owner);
+    function cancelAuction() public onlyOwner onlyBeforeEnd onlyNotCanceled returns (bool) {
+        canceled = true;
+        emit LogCanceled();
+        return true;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Caller is not the owner.");
         _;
     }
 
-    modifier onlyNotOwner {
-        require(msg.sender != owner);
+    modifier onlyNotOwner() {
+        require(msg.sender != owner, "Caller is the owner.");
         _;
     }
 
-    modifier onlyAfterStart {
-        require(block.number >= startBlock);
+    modifier onlyWinner() {
+        require(msg.sender == highestBidder, "Caller is not the owner.");
         _;
     }
 
-    modifier onlyBeforeEnd {
-        require (block.number <= endBlock);
+    modifier onlyAfterStart() {
+        require(block.number >= startBlock, "Auction has not started.");
         _;
     }
 
-    modifier onlyNotCanceled {
-        require(!canceled);
+    modifier onlyBeforeEnd() {
+        require(block.number <= endBlock, "Auction has ended.");
         _;
     }
 
-    modifier onlyEndedOrCanceled {
-        require(block.number >= endBlock || canceled);
+    modifier onlyNotCanceled() {
+        require(!canceled, "Auction has been canceled.");
+        _;
+    }
+
+    modifier onlyEndedOrCanceled() {
+        require(block.number >= endBlock || canceled, "Auction is not ended or canceled.");
+        _;
+    }
+
+    function min(uint a, uint b) internal pure returns (uint) {
+        return (a < b) ? a : b;
+    }
+
+    modifier onlyClaimed {
+        require(winnerHasClaimed);
         _;
     }
 }
